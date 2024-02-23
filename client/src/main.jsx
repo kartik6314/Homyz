9608c9af229460ef7702493fb81b22d694d810f2
KARTIK <kartik.barange@gmail.com>
2024-02-24 03:09:30 +0530

    aa

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       domain="dev-yuj17yzdv116pyon.us.auth0.com"
       clientId="YgChOssOghcfNRGcmjnTnZKlQoIb5HLr"
       authorizationParams={{
-        redirect_uri: "http://localhost:5173",
+        redirect_uri: "https://homyz-chi.vercel.app",
       }}
       audience="http://localhost:8000"
       scope="openid profile email"
